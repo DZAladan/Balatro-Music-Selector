e36cbac144c21a24cdf7255ad02997c3de2b86b8
DZAladan <48226177+DZAladan@users.noreply.github.com>
2025-10-05 22:03:05 +0200

    Create it.lua

---
+++ localization/it.lua
@@ -0,0 +1,16 @@
+return {
+	misc = {
+  dictionary = {
+   b_muselector = "Colonna sonora",
+   ml_muselector_opt={
+    "Balatro OST di LouisF",
+    "Cardsauce OST di Bassclefff",
+    "Bonne Soirée di Vongola",
+    "Monkey Business di Bombaflex",
+    "Balatro: B-side di Afterlight",
+    "Raise The Stakes di JohnathanSucks",
+    "Going in Blind di Recycled Scraps"
+   },
+  },
+ },
+}
