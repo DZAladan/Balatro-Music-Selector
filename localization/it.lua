b412f19709586ea991606c73d9fbdfcad069c46a
DZAladan <48226177+DZAladan@users.noreply.github.com>
2025-10-08 01:09:05 +0200

    Update it.lua

--- localization/it.lua
+++ localization/it.lua
@@ -3,13 +3,7 @@ return {
   dictionary = {
    b_muselector = "Colonna sonora",
    ml_muselector_opt={
-    "Balatro OST di LouisF",
-    "Cardsauce OST di Bassclefff",
-    "Bonne Soirée di Vongola",
-    "Monkey Business di Bombaflex",
-    "Balatro: B-side di Afterlight",
-    "Raise The Stakes di JohnathanSucks",
-    "Going in Blind di Recycled Scraps"
+    "Balatro OST - LouisF",
    },
   },
  },
