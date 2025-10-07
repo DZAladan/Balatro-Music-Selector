f37b3cbf6d839166b40b0c0a9a1131fb1e4ebf35
DZAladan <48226177+DZAladan@users.noreply.github.com>
2025-10-08 01:08:45 +0200

    Update en-us.lua

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -3,13 +3,7 @@ return {
   dictionary = {
    b_muselector = "Soundtrack",
    ml_muselector_opt={
-    "Balatro OST by LouisF",
-    "Cardsauce OST by Bassclefff",
-    "Bonne Soirée by Vongola",
-    "Monkey Business by Bombaflex",
-    "Balatro: B-side by Afterlight",
-    "Raise The Stakes by JohnathanSucks",
-    "Going in Blind by Recycled Scraps"
+    "Balatro OST - LouisF",
    },
   },
  },
